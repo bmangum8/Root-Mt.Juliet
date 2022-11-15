e7b6123db5d1504c1f4da238a18ae7e641444d63
Brittany Mangum <103229067+bmangum8@users.noreply.github.com>
2022-11-15 14:29:00 -0600

    Delete SQLQuery5.sql

--- SQLQuery5.sql
+++
@@ -1,6 +0,0 @@
-﻿ SELECT r.Id AS RequestId, r.TreeId, r.UserProfileId, r.DateCreated, r.DateCompleted,
-                                t.Name AS TreeName, up.Name AS UserProfileName, n.Name AS NeighborhoodName
-                            FROM Request r
-                            LEFT JOIN Tree t ON t.Id = r.TreeId
-                            LEFT JOIN UserProfile up ON up.Id = r.UserProfileId
-                            LEFT JOIN Neighborhood n ON n.Id = up.NeighborhoodId
